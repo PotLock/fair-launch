ac027718f44e76844df0d7f6ff238a8ced4f0ba3
louisdevzz <vohuunhan1310@gmail.com>
2025-11-13 03:35:12 +0700

    feat: update docs

--- docs/content/concepts-architecture/_meta.ts
+++ docs/content/concepts-architecture/_meta.ts
@@ -1,5 +1,5 @@
 export default {
-  'how-potlaunch-works': 'How Potlaunch Works',
+  'how-potlaunch-works': 'How POTLAUNCH Works',
   'launch-flow-architecture': 'Launch Flow Architecture',
   'token-lifecycle': 'Token Lifecycle',
   'anti-rug-mechanisms': 'Anti-rug Mechanisms',
